7d47022ff99e8785ee0d55de8ffed5d11dc95bad
sowmya <cloud.sowmya@gmail.com>
2024-05-27 19:13:22 +0530

    Required terraform files

---
+++ hellow.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+yum update -y
+yum -y install httpd
+cat <<EOF > /var/www/html/index.html
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Portfolio</title>
+  <style>
+    /* Add animation and styling for the text */
+    @keyframes colorChange {
+      0% { color: red; }
+      50% { color: green; }
+      100% { color: blue; }
+    }
+    h1 {
+      animation: colorChange 2s infinite;
+    }
+  </style>
+</head>
+<body>
+  <h1>Terraform Project Server 1</h1>
+  <p>Hello Maxx,This is for you :)</p>
+  
+</body>
+</html>
+EOF
+systemctl start httpd
+systemctl enable httpd
